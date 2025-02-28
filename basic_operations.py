e85851dba888629427ab2b2a8a79e28e06deed31
FatimahAnimashaun <153679058+FatimahAnimashaun@users.noreply.github.com>
2025-02-28 11:08:02 +0100

    Add files via upload

---
+++ basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+# Performing arithmetic operations
+addition = number1 + number2
+subtraction = number2 - number1
+multiplication = number1 * number2
+
+# Printing the results
+print(f"Sum of {number1} and {number2} is {addition}")
+print(f"Difference of {number2} and {number1} is {subtraction}")
+print(f"Product of {number1} and {number2} is {multiplication}")
